52b9a98d38a0c23625193004e4a73d13df7e6f19
xsyetopz <187086553+xsyetopz@users.noreply.github.com>
2026-06-08 16:02:56 +0300

    remove almost all python bits

--- src/_dist.bat
+++
@@ -1,3 +0,0 @@
-pyinstaller "gui.py" --onefile --clean --distpath "" --icon="app/icon.ico" --add-data="resources;resources" --add-data="icons;icons" --add-data="app;app"
-@RD /S /Q "build"
-rename gui.exe pscoverdl.exe
